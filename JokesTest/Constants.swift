6d9c8dc59f678e4667aeab10b016aa3132f74331
Warren Hansen <whansen1@mac.com>
2017-09-26 08:04:48 -0700

    task: basic view controllers

---
+++ JokesTest/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  JokesTest
+//
+//  Created by Warren Hansen on 9/26/17.
+//  Copyright © 2017 Warren Hansen. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "https://mtdash01.firebaseio.com"
